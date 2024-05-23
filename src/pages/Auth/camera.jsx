8a212a6a87d73090d8060b9dc71442f8f12b8934
egon6018 <goeun6018@naver.com>
2024-05-23 16:21:13 +0900

    화면구상중

--- src/pages/Auth/camera.jsx
+++ src/pages/Auth/camera.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState, useRef } from "react";
-import './camera.scss';
+import "./camera.scss";
 function Camera() {
+
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
+  const [capturedImage, setCapturedImage] = useState(null);
 
   useEffect(() => {
     const getCameraStream = async () => {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({
-          //video: true // 전면카메라
+          video: true // 전면카메라
 
-          video: {
-            facingMode: { exact: "environment" } // 후면 카메라 사용 설정
-          }
+          // video: {
+          //   facingMode: { exact: "environment" } // 후면 카메라 사용 설정
+          // }
         });
-        videoRef.current.srcObject = stream;
+        videoRef.current.srcObject = stream;     
       } catch (error) {
         console.error("Error accessing camera:", error);
       }
@@ -31,23 +33,27 @@ function Camera() {
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
     const dataURL = canvas.toDataURL("image/png");
     console.log(dataURL);
+    setCapturedImage(dataURL); // 사진 저장
   };
 
   return (
-    <div className="camera-container">
-      <p className="text-container">
-        신분증 인증
-      </p>
+    <div className="camera-container">    
+      <p className="text-container">신분증 인증</p>
       <div className="camera-preview">
         <video ref={videoRef} autoPlay playsInline></video>
         <div className="overlay"></div>
       </div>
       <p className="text-container">
-        영역 안에 신분증이 꽉 차도록 배치 후 <br/>
+        영역 안에 신분증이 꽉 차도록 배치 후 <br />
         하단 버튼을 누르면 촬영됩니다.
       </p>
       <button className="capture-button" onClick={capturePhoto}></button>
       <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
+      {capturedImage && (
+        <div className="captured-image-container">
+          <img src={capturedImage} alt="Captured" />
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
-import './index.scss';
+import React, { useState } from "react";
+import "./index.scss";
+import Button from "@/components/Button";
 
 function Auth() {
-  const [name, setName] = useState('');
-  const [registNumber, setRegistNumber] = useState('');
-  const [address, setAddress] = useState('');
-  const [issueDate, setIssueDate] = useState('');
+  const [name, setName] = useState("");
+  const [registNumber, setRegistNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [issueDate, setIssueDate] = useState("");
 
   return (
     <div className="Auth">
       <div className="container">
-        <h2 className='text-container'>신분증 인증</h2>
+        <h2 className="text-container">신분증 인증</h2>
         <div className="id-card">
           <img src="/path/to/your/id-card-image.png" alt="ID Card" />
         </div>
-        <p className='text-container'>
-            신분증 정보를 확인해주세요. <br/><br/><br/><br/>
-            신분증 정보를 확인하고 지금 수동으로 작성해주세요. <br/>
-            실제 정보와 다른 경우 등록이 불가합니다.
+        <p className="text-container">
+          신분증 정보를 확인해주세요. <br />
+          <br />
+          <br />
+          <br />
+          신분증 정보를 확인하고 지금 수동으로 작성해주세요. <br />
+          실제 정보와 다른 경우 등록이 불가합니다.
         </p>
         <form>
           <label>
@@ -42,7 +46,7 @@ function Auth() {
               type="text"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
-            />          
+            />
           </label>
           <label>
             발급일자
@@ -52,6 +56,13 @@ function Auth() {
               onChange={(e) => setIssueDate(e.target.value)}
             />
           </label>
+
+          <Button size="large" shape="rect" onClick={""}>
+            확인
+          </Button>
+          <Button size="large" shape="rect" onClick={""}>
+            취소
+          </Button>
         </form>
       </div>
     </div>
