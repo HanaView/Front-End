207600ebe9189d9f417ba8a364ebb1f49f0aee90
yunred <nado0903@gmail.com>
2024-05-23 14:05:00 +0900

    Feat: 배포 테스트를 위한 버튼 추가

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getTest } from "@/apis/user";
+import Button from "@/components/Button";
+import { useNavigate } from "react-router-dom";
 
 function Main() {
+  const navigate = useNavigate();
   const { data } = useQuery({
     queryKey: ["test"],
     queryFn: () => getTest()
@@ -10,7 +13,13 @@ function Main() {
 
   if (!data) return <></>;
 
-  return <>{<p>{data.description}</p>}</>;
+  return (
+    <>
+      <p>{data.description}</p>
+      <Button onClick={() => navigate("/test")}>TEST</Button>
+      <Button onClick={() => navigate("/auth")}>CAMERA</Button>
+    </>
+  );
 }
 
 export default Main;
