fe5b0db711740a9774fb4bdba9ae432df4e4442a
allzeroyou <ekdudd119@naver.com>
2024-05-28 17:27:57 +0900

    feat: 화면공유/화상통화 video 화면 컴포넌트화

--- src/pages/Consulting/Customer/index.jsx
+++ src/pages/Consulting/Customer/index.jsx
@@ -1,34 +1,13 @@
-// 손님 화상 상담 화면
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "./index.scss";
-import Button from "@/components/Button";
-import { useAtom } from "jotai";
-import { capturedImageAtom } from "@/stores";
-//rcfe
+import React from "react";
+import Consulting from "@/components/Video/index";
+import "./index.scss"; // customer/index.jsx의 스타일 파일
 
-function Consulting() {
+const CustomerPage = () => {
   return (
-    <div className="Consulting">
-      <div className="connection-options">
-        <div className="option">
-          <div className="section face">
-            <div className="title">화상연결</div>
-            <img></img>
-          </div>
-          <div className="section share">
-            <div className="title">화면공유</div>
-            <img></img>
-          </div>
-        </div>
-      </div>
-      <div className="content">
-        <div className="video-section">
-          <img></img>
-        </div>
-      </div>
+    <div className="CustomerPage">
+      <Consulting />
     </div>
   );
-}
+};
 
-export default Consulting;
+export default CustomerPage;
