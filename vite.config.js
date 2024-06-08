cf833be4bdcd3103a6421994c65de17740d1ebda
allzeroyou <ekdudd119@naver.com>
2024-06-09 01:19:16 +0900

    feat: https aws 서버 배포에 따라 임시로 https 설정해둔 mkcert 라이브러리 삭제

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [react()],
 
   resolve: {
     alias: {
@@ -26,7 +25,5 @@ export default defineConfig({
   }
   ,server:{
     host:"0.0.0.0",
-    https: true
-
   }
 });
