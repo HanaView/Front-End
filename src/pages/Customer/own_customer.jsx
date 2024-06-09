f23e42ad0cf8b91d1e75da43f2fad0cf6d2f0789
yunred <nado0903@gmail.com>
2024-06-09 20:38:23 +0900

    Feat: 마이자산 API연동

--- src/pages/Customer/own_customer.jsx
+++ src/pages/Customer/own_customer.jsx
@@ -33,27 +33,27 @@ const data = {
 };
 
 function OwnCustomer() {
-  // const { data: userDeposits } = useQuery({
-  //   queryKey: ["getUserDeposits"],
-  //   queryFn: () => getUserDeposits(1)
-  // });
+  const { data: userDeposits } = useQuery({
+    queryKey: ["getUserDeposits"],
+    queryFn: () => getUserDeposits(1)
+  });
 
-  // const { data: userCards } = useQuery({
-  //   queryKey: ["getUserCards"],
-  //   queryFn: () => getUserCards(1)
-  // });
+  const { data: userCards } = useQuery({
+    queryKey: ["getUserCards"],
+    queryFn: () => getUserCards(1)
+  });
 
-  // const { data: userSavings } = useQuery({
-  //   queryKey: ["getUserSavings"],
-  //   queryFn: () => getUserSavings(1)
-  // });
+  const { data: userSavings } = useQuery({
+    queryKey: ["getUserSavings"],
+    queryFn: () => getUserSavings(1)
+  });
 
-  // useEffect(() => {
-  //   console.log("userDeposits", userDeposits);
-  //   console.log("userCards", userCards);
-  //   console.log("userSavings", userSavings);
-  // }, [userDeposits, userCards, userSavings]);
-  // if (!userCards) return <></>;
+  useEffect(() => {
+    console.log("userDeposits", userDeposits);
+    console.log("userCards", userCards);
+    console.log("userSavings", userSavings);
+  }, [userDeposits, userCards, userSavings]);
+  if (!userCards) return <></>;
 
   return (
     <>
@@ -68,66 +68,62 @@ function OwnCustomer() {
           <div className="productContainer">
             <div className="productTitle">예금</div>
             <div className="product">
-              <div>369정기예금</div>
-              <div>영하나플러스</div>
+              {userDeposits.data?.map((product, index) => (
+                <div key={index}>{product.depositInfo?.name}</div>
+              ))}
             </div>
             <div className="productTitle">적금</div>
             <div className="product">
-              <div>펫사랑 적금</div>
-              <div>(내맘) 적금</div>
+              {userSavings.data?.map((product, index) => (
+                <div key={index}>{product.parent?.depositInfo.name}</div>
+              ))}
             </div>
             <div className="productTitle">카드</div>
             <div className="product">
-              <div>트래블로그 체크카드</div>
-              <div>트래블로그 신용카드</div>
+              {userCards.data?.map((product, index) => (
+                <div key={index}>{product.cardInfo?.name}</div>
+              ))}
             </div>
             <div className="productTitle">대출</div>
-            <div className="product">
-
-            </div>
+            <div className="product"></div>
           </div>
         </div>
       </div>
     </>
 
-
-
-        //  <>
-        //   <div className="wrapper ownCustomerTable">
-        //     <div className="table">
-        //       <div className="row header green">
-        //         <div className="cell">카드상품명</div>
-        //       </div>
-        //        {userCards.data.map((product, index) => (
-        //         <div className="row" key={index}>
-        //           <div className="cell" data-title="상품명">
-        //             {product.cardInfo.name}
-        //           </div>
-        //         </div>
-        //       ))} 
-        //     </div>
-        //   </div>
-        // </>
-        // <>
-        //   <div className="wrapper ownCustomerTable">
-        //     <div className="table">
-        //       <div className="row header green">
-        //         <div className="cell">예금 계좌</div>
-        //       </div>
-        //       {userDeposits.data.map((product, index) => (
-        //         <div className="row" key={index}>
-        //           <div className="cell" data-title="상품명">
-        //             {product.accountNumber}
-        //           </div>
-        //         </div>
-        //       ))} 
-        //     </div>
-        //   </div>
-        // </> 
-        // </div> 
-
-
-  
+    //  <>
+    //   <div className="wrapper ownCustomerTable">
+    //     <div className="table">
+    //       <div className="row header green">
+    //         <div className="cell">카드상품명</div>
+    //       </div>
+    //        {userCards.data.map((product, index) => (
+    //         <div className="row" key={index}>
+    //           <div className="cell" data-title="상품명">
+    //             {product.cardInfo.name}
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </>
+    // <>
+    //   <div className="wrapper ownCustomerTable">
+    //     <div className="table">
+    //       <div className="row header green">
+    //         <div className="cell">예금 계좌</div>
+    //       </div>
+    //       {userDeposits.data.map((product, index) => (
+    //         <div className="row" key={index}>
+    //           <div className="cell" data-title="상품명">
+    //             {product.accountNumber}
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </>
+    // </div>
   );
 }
 export default OwnCustomer;
